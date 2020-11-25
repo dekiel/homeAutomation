bc7b85356cc3046d9969e2f926be685b6ab9955e
dekiel <przemyslaw.pokrywka@sap.com>
2020-11-25 11:41:37 +0100

    open for write

--- gpio.py
+++ gpio.py
@@ -3,20 +3,21 @@ app = Flask(__name__)
 
 @app.route('/gabinet-on')
 def gabinetOn():
-    with open('/sys/class/gpio/gpio102/value') as f:
+    with open('/sys/class/gpio/gpio102/value', 'w') as f:
         f.write("1")
     return 'ogrzewanie w gabinecie wlaczone'
 @app.route('/gabinet-off')
 def gabinetOff():
-    with open('/sys/class/gpio/gpio102/value') as f:
+    with open('/sys/class/gpio/gpio102/value', 'w') as f:
         f.write("0")
     return 'ogrzewanie w gabinecie wylaczone'
 @app.route('/gabinet-status')
 def gabinetStatus():
     with open('/sys/class/gpio/gpio102/value') as f:
         status = f.read()
-    if status == "1":
-        return "ogrzewanie w gabinecie jest wlaczone"
-    if status == "0":
-        return "ogrzewanie w gabinecie jest wylaczone"
-    return "status ogrzewania w gabinecie nieznany"
+    #if status == "1":
+        #return "ogrzewanie w gabinecie jest wlaczone"
+    #if status == "0":
+    #    return "ogrzewanie w gabinecie jest wylaczone"
+    return status
+    #return "status ogrzewania w gabinecie nieznany"
