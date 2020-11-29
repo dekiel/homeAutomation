18e3dc7aa4063521f79ba5b5d495a1a6f650c874
dekiel <przemyslaw.pokrywka@sap.com>
2020-11-29 18:54:18 +0100

    fix unit files

--- resetWatchDog.py
+++ resetWatchDog.py
@@ -7,6 +7,6 @@ UDP_IP = "192.168.255.123"
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 if __name__ == '__main__':
-    sock.sendto(bytes('live', 'utf-8'), (UDP_IP, 9998))
+    sock.sendto(bytes('live', 'utf-8'), (UDP_IP, 9999))
     with open('/root/webAPI/status', 'w') as f:
         f.write("live")
