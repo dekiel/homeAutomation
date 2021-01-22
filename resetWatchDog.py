9b0f1b9c5d31c5defdc647d880725bd6f91b8ac3
dekiel <przemyslaw.pokrywka@sap.com>
2021-01-22 20:59:54 +0100

    fix typos and errors

--- resetWatchDog.py
+++ resetWatchDog.py
@@ -8,5 +8,5 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 if __name__ == '__main__':
     sock.sendto(bytes('live', 'utf-8'), (UDP_IP, 9999))
-    with open('/root/webAPI/status', 'w') as f:
+    with open('/home/rock64/webAPI/status', 'w') as f:
         f.write("live")
